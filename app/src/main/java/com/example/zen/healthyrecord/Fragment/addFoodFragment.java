46410d28a69fc3587baa75dd89f3dc7081807b20
sharon <u8528816@gmail.com>
2017-03-20 20:14:29 +0800

    third

---
+++ app/src/main/java/com/example/zen/healthyrecord/Fragment/addFoodFragment.java
@@ -0,0 +1,16 @@
+package com.example.zen.healthyrecord.Fragment;
+
+import android.os.Bundle;
+
+/**
+ * Created by sharonyu on 2017/3/20.
+ */
+
+public class addFoodFragment extends addItemPage {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    
+}
