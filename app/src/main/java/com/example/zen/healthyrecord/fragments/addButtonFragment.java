ccfe4aab8e31df1cf16a8940e6c87fd145d00bd2
sharon <u8528816@gmail.com>
2017-03-21 13:29:01 +0800

    add drawerLayout and change button Fragment

--- app/src/main/java/com/example/zen/healthyrecord/fragments/addButtonFragment.java
+++ app/src/main/java/com/example/zen/healthyrecord/fragments/addButtonFragment.java
@@ -4,44 +4,38 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
-
+import com.example.zen.healthyrecord.AddItemActivity;
 import com.example.zen.healthyrecord.R;
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link addItemPage.OnFragmentInteractionListener} interface
+ * {@link addButtonFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link addItemPage#newInstance} factory method to
+ * Use the {@link addButtonFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class addItemPage extends Fragment{
+public class addButtonFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    TextView txtDatePicker;
-    TextView txtTimePicker;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
-
-    String dateStr = "04/05/2010";
+    Button btnSport;
+    Button btnFood;
 
     private OnFragmentInteractionListener mListener;
 
-    public addItemPage() {
+    public addButtonFragment() {
         // Required empty public constructor
     }
 
@@ -51,26 +45,25 @@ public class addItemPage extends Fragment{
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment addItemPage.
+     * @return A new instance of fragment addButtonFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static addItemPage newInstance(String param1, String param2) {
-        addItemPage fragment = new addItemPage();
+    public static addButtonFragment newInstance(String param1, String param2) {
+        addButtonFragment fragment = new addButtonFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+        args.putString( ARG_PARAM1, param1 );
+        args.putString( ARG_PARAM2, param2 );
+        fragment.setArguments( args );
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate( savedInstanceState );
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mParam1 = getArguments().getString( ARG_PARAM1 );
+            mParam2 = getArguments().getString( ARG_PARAM2 );
         }
-
 
     }
 
@@ -78,51 +71,43 @@ public class addItemPage extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_add_item_page, container, false);
 
-        txtDatePicker = (TextView)v.findViewById(R.id.txtDatePicker);
-        txtTimePicker = (TextView)v.findViewById(R.id.txtTimePicker);
+        View v = inflater.inflate( R.layout.fragment_add_button, container, false );
+        btnFood = (Button) v.findViewById(R.id.btnDiet);
+        btnSport = (Button) v.findViewById(R.id.btnSport);
+        btnFood.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-        Calendar c = Calendar.getInstance();
-        SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
-        SimpleDateFormat tf = new SimpleDateFormat("HH:mm");
-        String formattedDate = df.format(c.getTime());
-        String formattedTime = tf.format(c.getTime());
-
-
-        txtDatePicker.setText(formattedDate);
-        txtTimePicker.setText(formattedTime);
-
+                ((AddItemActivity)getActivity()).changeFragment();
+            }
+        } );
         return v;
-
-
     }
 
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+            mListener.onFragmentInteraction( uri );
         }
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach( context );
+//        if (context instanceof OnFragmentInteractionListener) {
+//            mListener = (OnFragmentInteractionListener) context;
+//        } else {
+//            throw new RuntimeException( context.toString()
+//                    + " must implement OnFragmentInteractionListener" );
+//        }
+//    }
 
     @Override
     public void onDetach() {
         super.onDetach();
         mListener = null;
     }
-
-
 
     /**
      * This interface must be implemented by activities that contain this
@@ -138,6 +123,4 @@ public class addItemPage extends Fragment{
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
-
-
 }
